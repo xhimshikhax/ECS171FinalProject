5980f317541463bd7670a7bfbc1b88c6f7cc2c72
Taj <sharma.tajinder01@gmail.com>
2023-05-30 22:31:05 -0700

    Change .data file to correct .css file

--- flaskfrontend/main.py
+++ flaskfrontend/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, url_for, render_template , request
 import pandas as pd
+from model import train_classifier, predict_efficiency
 app = Flask(__name__)
+
+clf = train_classifier('project.csv')
 
 @app.route("/")
 def home():
@@ -15,9 +18,13 @@ def predict():
     weight = float(request.form["weight"])
     acceleration = float(request.form["acceleration"])
 
-    efficient_car = False
+    car_data = [cylinders, displacement, horsepower, weight, acceleration] # input to your model
+    prediction = predict_efficiency(clf, car_data)
 
-    return render_template("prediction.html",efficient_car = efficient_car)
+    # Your model predicts 1 for efficient and 0 for not efficient
+    efficient_car = prediction == 1
+
+    return render_template("prediction.html", efficient_car = efficient_car)
 
 if __name__ == "__main__":
     app.run()
